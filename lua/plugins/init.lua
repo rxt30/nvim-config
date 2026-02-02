2a5708cb91a0959c06f1eba46c203b33af8ada7d
rxt30 <mhampel@pm.me>
2026-02-02 09:00:48 +0100

    refactor: formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,33 +1,33 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		error("Error cloning lazy.nvim:\n" .. out)
-	end
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({
-	"tpope/vim-sleuth",
-	"hiphish/rainbow-delimiters.nvim",
-	"catgoose/nvim-colorizer.lua",
-	"voldikss/vim-floaterm",
-	require("plugins.gitsigns"),
-	require("plugins.which-key"),
-	require("plugins.telescope"),
-	require("plugins.lsp"),
-	require("plugins.theme"),
-	require("plugins.mini"),
-	require("plugins.treesitter"),
-	require("plugins.lualine"),
-	require("plugins.window-picker"),
-	require("plugins.autocompletion"),
-	require("plugins.copilot"),
-	require("plugins.gitlab"),
-	require("plugins.gitsigns"),
-	require("plugins.indent_line"),
-	require("plugins.lint"),
-	require("plugins.neo-tree"),
-	-- require("plugins.obsidian"),
-	require("plugins.typescript"),
-})
+require('lazy').setup {
+  'tpope/vim-sleuth',
+  'hiphish/rainbow-delimiters.nvim',
+  'catgoose/nvim-colorizer.lua',
+  'voldikss/vim-floaterm',
+  require 'plugins.gitsigns',
+  require 'plugins.which-key',
+  require 'plugins.telescope',
+  require 'plugins.lsp',
+  require 'plugins.theme',
+  require 'plugins.mini',
+  require 'plugins.treesitter',
+  require 'plugins.lualine',
+  require 'plugins.window-picker',
+  require 'plugins.autocompletion',
+  require 'plugins.copilot',
+  require 'plugins.gitlab',
+  require 'plugins.gitsigns',
+  require 'plugins.indent_line',
+  require 'plugins.lint',
+  require 'plugins.neo-tree',
+  -- require("plugins.obsidian"),
+  require 'plugins.typescript',
+}
