aa2e0a8d539e58ba56317087bbbf6239ae08210f
rxt30 <mhampel@pm.me>
2026-05-28 11:13:40 +0200

    refactor(treesitter): main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	main = "nvim-treesitter.configs",
-	opts = {
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		indent = { enable = true },
-	},
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  branch = 'main',
+  opts = {
+    auto_install = true,
+    highlight = {
+      enable = true,
+    },
+    indent = { enable = true },
+  },
 }
